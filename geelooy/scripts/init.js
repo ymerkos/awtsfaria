8a65c326a2ef6a5a5e4736eb397ee23813848f42
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-06-20 20:56:38 -0400

    B"H

---
+++ geelooy/scripts/init.js
@@ -0,0 +1,10 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyCgWlJYMHUYKp-oQgi0wlmOA2TJ8q6g2Ic",
+  "appId": "1:599561241377:web:aa8dc10597e27b3badb460",
+  "authDomain": "awtsmoosaria.firebaseapp.com",
+  "databaseURL": "https://awtsmoosaria-default-rtdb.firebaseio.com",
+  "messagingSenderId": "599561241377",
+  "projectId": "awtsmoosaria",
+  "storageBucket": "awtsmoosaria.appspot.com"
+});
