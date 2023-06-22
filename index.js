52c723cc86bf1f5621685575de730548a39e1636
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-06-21 20:08:25 -0400

    B"H

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //B"H
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises; // Use promises version of fs
 const path = require('path');
 const processTemplate = require('./ayzarim/awtsmoosProcessor.js');
 const DosDB = require("./ayzarim/DosDB.js")
@@ -11,7 +11,7 @@ const mimeTypes = {
     '.css': 'text/css'
 };
 
-http.createServer((request, response) => {
+http.createServer(async (request, response) => { // Make request handler async
     let filePath = './geelooy' + request.url;
     if (filePath == './geelooy/') {
         filePath = './geelooy/index.html';
@@ -24,25 +24,18 @@ http.createServer((request, response) => {
 
     const contentType = mimeTypes[extname] || 'application/octet-stream';
 
-    fs.readFile(filePath, (errors, content) => {
-        if (!errors) {
-            var processed;
-            try {
-                processed = processTemplate(content, {
-                    DosDB
-                });
-            } catch(e) {
-                processed = JSON.stringify({errorWasHere:e})
-            }
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(processed, 'utf-8');
-        } else {
-            console.log(errors);
-            response.writeHead(404, { 'Content-Type': 'text/html' });
-            response.end("B\"H<br>There were some errors! Time for Teshuva :)<br>"+JSON.stringify(errors));
-        }
-    });
-
+    try {
+        const content = await fs.readFile(filePath, 'utf-8'); // Await readFile
+        const processed = await processTemplate(content, { // Await processTemplate
+            DosDB
+        });
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(processed, 'utf-8');
+    } catch (errors) {
+        console.log(errors);
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end("B\"H<br>There were some errors! Time for Teshuva :)<br>"+JSON.stringify(errors));
+    }
 }).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080/');
