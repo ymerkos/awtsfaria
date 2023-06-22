ab68696fd67100af2a2be48bffd945a8e4f459e4
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-06-21 20:07:19 -0400

    B"H

--- ayzarim/awtsmoosProcessor.js
+++ ayzarim/awtsmoosProcessor.js
@@ -1,11 +1,12 @@
 // B"H
 const vm = require('vm');
 
-function processTemplate(template) {
-    return template.replace(/<\?Awtsmoos([^>]+)\?>/g, (match, code) => {
+async function processTemplate(template) {
+    return await template.replace(/<\?Awtsmoos([^>]+)\?>/g, async (match, code) => {
         const script = new vm.Script(code);
         const context = vm.createContext({});
-        return script.runInContext(context);
+        var tawmtsees /*result*/ =  script.runInContext(context);
+        return await Promise.resolve(tawmtsees)
     });
 }
 
