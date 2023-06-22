cdf99c0211cee8694c9842686374abca1f4330c6
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-06-21 20:12:10 -0400

    B"H

--- ayzarim/awtsmoosProcessor.js
+++ ayzarim/awtsmoosProcessor.js
@@ -1,13 +1,25 @@
 // B"H
 const vm = require('vm');
 
-async function processTemplate(template) {
-    return await template.replace(/<\?Awtsmoos([^>]+)\?>/g, async (match, code) => {
+async function processTemplate(template, context = {}) {
+    // Split the template into segments based on your tags
+    const segments = template.split(/<\?Awtsmoos|\?>/g);
+
+    // Process each segment asynchronously
+    for (let i = 1; i < segments.length; i += 2) {
+        const code = segments[i];
         const script = new vm.Script(code);
-        const context = vm.createContext({});
-        var tawmtsees /*result*/ =  script.runInContext(context);
-        return await Promise.resolve(tawmtsees)
-    });
+        const result = script.runInContext(vm.createContext(context));
+        // If result is a Promise, await it
+        if (result instanceof Promise) {
+            segments[i] = await result;
+        } else {
+            segments[i] = result;
+        }
+    }
+
+    // Reassemble the template
+    return segments.join('');
 }
 
 module.exports = processTemplate;
