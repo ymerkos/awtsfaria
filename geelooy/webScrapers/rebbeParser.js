bdae63b69fe3aaf4a6a32fb98b718293a1dc2e5d
ymerkos <ykaufer@merkos302.com>
2023-08-02 01:06:36 -0400

    BH Igeres Hakodesh + Rebbe's Torah in general parser

---
+++ geelooy/webScrapers/rebbeParser.js
@@ -0,0 +1,179 @@
+//B"H
+/**
+chabad library parser
+
+start https://chabadlibrary.org/books/admur/index.htm for now
+**/
+
+var p = new DOMParser();
+async function parseRebbe() {
+    var books = {};
+    var lnk = Array.from(document.querySelectorAll("a.toc"));
+    var mn/*main*/=await getDir();
+    var dir = await mkdir("books",mn);
+    console.log("got main");
+    var i;
+    for(
+        i=0;
+        i<lnk.length;
+        i++
+    ) {
+        var sub = await getSubSection(
+            lnk[i],dir
+        );
+        books[lnk[i].textContent]=sub;
+    }
+    return books;
+}
+
+async function getSubSection(a,dir) {
+    var data = {};
+    var g = await getDoc(a.href)
+    var subLinks = Array.from(g.querySelectorAll("a.toc"));
+    var sub = await mkdir(
+        rn(a.textContent), dir
+    );
+
+    console.log("got sub sec",a.textContent);
+    var i;
+    for(
+        i=0;
+        i<subLinks.length;
+        i++
+    ) {
+        var subSub = await getSubSubSection(
+            subLinks[i], sub
+        );
+        data[a.textContent]=subSub
+    }
+}
+
+async function getSubSubSection(f, dr) {
+    var pages = {};
+    var d = await getDoc(f.href);
+    var a = Array.from(d.querySelectorAll("a.toc"));
+    var sub = await mkdir(
+        rn(f.textContent),
+        dr
+    );
+    console.log("got sub sub sec",f.textContent);
+    var i;
+    for(
+        i=0;
+        i<a.length;
+        i++
+    ) {
+        var page = await parsePage(
+            a[i], sub
+        );
+        pages[f.textContent]=page;
+    }
+}
+
+async function parsePage(a, dir) {
+    var d = await getDoc(a.href);
+    var inner = d.getElementById("inner-content");
+    if(inner) {
+        var c = pageToJsonAwtsmoosed(inner);
+        var f= await mkfl(
+            rn(a.textContent)+".json",
+            dir,
+            JSON.stringify(c)
+        );
+        console.log("writing",a.textContent);
+        return c;
+    }
+
+    return null;
+}
+
+function pageToJsonAwtsmoosed(ic/*innerContent*/) {
+    var info = {}
+    var pages = {};
+    info.pages=pages;
+    var curPage = null;
+    Array.from(ic.children).forEach(q=>{
+        if(
+            q.className == "pagenumber" ||
+            q.className == "pageno"
+        ) {
+            curPage = q.textContent;
+            pages[curPage] = [];
+            return;
+        }
+
+        if(curPage === null) return;
+
+        if(q.nodeName == "P")
+            pages[curPage].push(q.outerHTML);
+    });
+    var footes = Array.from(ic.querySelectorAll("p.ftntext"));
+    footes.forEach(w=>{
+        if(!info.footnotes) {
+            info.footnotes = {};
+        }
+
+        var a = w.children[0];
+        if(a) {
+            var nm = a.name;
+            if(nm) {
+                var num = nm.substring(5);
+                if(num != nm) {
+                    info.footnotes[num] = a.innerHTML;
+                }
+            }
+        }
+    });
+
+    return info;
+}
+
+var rnd = 0;
+function rn/*rename better*/(t) {
+    if(t && typeof(t) == "string")
+        return t.split(" ")
+        .join("_")
+        .split('"')
+        .join("-");
+    return "BH_"+Date.now()+"_"+(rnd++);
+}
+async function getDoc(href) {
+    var p = new DOMParser();
+    var g = await fetch(href);
+    var t = await g.text()
+    var s = p.parseFromString(t,"text/html")
+    return s;
+}
+
+async function getDir() {
+    return await showDirectoryPicker();
+}
+
+async function mkdir(nm, dir) {
+    return await dir.getDirectoryHandle(nm, {
+        create:true
+    });
+}
+
+/**
+ * 
+ * @param {String} nm
+ *  the name of the file to make 
+ * @param {DirectorySystemHandle} dir
+ * the parent dir 
+ * @param {String} txt
+ * content 
+ * @returns FileSystemHandle 
+ */
+async function mkfl(nm, dir, txt) {
+    var fh = await dir.getFileHandle(nm, {
+        create:true
+    });
+    var w = await fh.createWritable();
+    console.log("About to write to",nm,txt,w)
+    await w.write(txt);
+    await w.close();
+    return fh;
+}
+
+books = await parseRebbe()
