dbab0f193c71ea4fca1db64463ac462d165df3e4
ymerkos <ykaufer@merkos302.com>
2023-08-04 16:04:19 -0400

    BH GraphDB version 2

--- ayzarim/GraphDB.js
+++ ayzarim/GraphDB.js
@@ -26,146 +26,87 @@ class Relationship {
 
 
 
+
 class GraphDB extends DosDB {
     constructor(directory, cacheSize = 1000) {
         super(directory);
-        this.indexes = {
-            nodeType: {},
-            relationshipType: {}
-        };
         this.cache = new Map();
         this.cacheSize = cacheSize;
+        this.indices = {
+            nodeType: new Map(),
+            relationshipType: new Map()
+        };
+        this.loadIndices().catch(console.error);
     }
 
-    async get(id) {
-        // Check the cache first
-        if (this.cache.has(id)) {
-          // Move the accessed item to the end of the map
-          const value = this.cache.get(id);
-          this.cache.delete(id);
-          this.cache.set(id, value);
-          return value;
+    async createUUID() {
+        let id = uuidv4();
+        while (await this.exists(id)) {
+            id = uuidv4();
         }
-    
-        const value = await super.get(id);
-    
-        // If the cache is full, remove the least recently used item
-        if (this.cache.size >= this.cacheSize) {
-          const firstKey = this.cache.keys().next().value;
-          this.cache.delete(firstKey);
-        }
-    
-        // Add the item to the cache
-        this.cache.set(id, value);
-    
-        return value;
-      }
-
-    async loadAllDataAndBuildIndexes() {
-        const files = await fs.readdir(this.directory);
-        for (let file of files) {
-            const id = path.basename(file, '.json');
-            const data = await this.get(id);
-
-            if (data instanceof Node) {
-                // Update the nodeType index
-                if (!this.indexes.nodeType[data.data.type]) {
-                    this.indexes.nodeType[data.data.type] = [];
-                }
-                this.indexes.nodeType[data.data.type].push(id);
-            } else if (data instanceof Relationship) {
-                // Update the relationshipType index
-                if (!this.indexes.relationshipType[data.type]) {
-                    this.indexes.relationshipType[data.type] = [];
-                }
-                this.indexes.relationshipType[data.type].push({src: data.srcId, dest: data.destId});
-            }
-        }
+        return id;
     }
 
-
-    
-    async loadIndexes() {
-        try {
-            this.indexes.nodeType = await this.get('index/nodeType') || {};
-            this.indexes.relationshipType = await this.get('index/relationshipType') || {};
-        } catch (error) {
-            console.error('Error loading indexes:', error);
-        }
-    }
-
-    async saveIndexes() {
-        try {
-            await this.create('index/nodeType', this.indexes.nodeType);
-            await this.create('index/relationshipType', this.indexes.relationshipType);
-        } catch (error) {
-            console.error('Error saving indexes:', error);
-        }
-    }
-
-    async updateIndex(type, id, dir) {
-        let index;
-        try {
-            index = await this.get(`index/${type}`);
-        } catch (error) {
-            index = [];
-        }
-        index.push(id);
-        await this.create(`index/${type}`, index);
-        await this.create(`${dir}/${id}`, index);
-    }
-    
-    
     async createNode(data) {
-        const id = uuidv4();
+        const id = await this.createUUID();
         const node = new Node(id, data);
-        await this.create(`node/${id}`, node);
-
-        // Update the nodeType index
-        await this.updateIndex(data.type, id, 'node');
-       
-
+        await this.create(id, node);
+        this.updateIndex('nodeType', data.type, id);
         return id;
     }
 
     async createRelationship(srcId, destId, type, data) {
+        const id = await this.createUUID();
         const relationship = new Relationship(srcId, destId, type, data);
-        const key = `${srcId}/${destId}/${type}`;
-        await this.create(key, relationship);
-
-        // Update the relationshipType index
-        await this.updateIndex(type, key, 'relationship');
-    
-
-        return key;
+        await this.create(id, relationship);
+        this.updateIndex('relationshipType', type, id);
+        return id;
     }
 
+    async updateIndex(indexType, type, id) {
+        let indexList = this.indices[indexType].get(type);
+        if (!indexList) {
+            indexList = [];
+            this.indices[indexType].set(type, indexList);
+        }
+        indexList.push(id);
+        await this.save(`index/${indexType}/${type}`, indexList);
+    }
 
     async getNodesOfType(type) {
-        const nodeIds = this.indexes.nodeType[type] || [];
-        return Promise.all(nodeIds.map(id => this.get(id)));
+        const nodeIds = this.indices.nodeType.get(type);
+        if (!nodeIds) {
+            return [];
+        }
+        return await Promise.all(nodeIds.map(id => this.get(id)));
     }
 
     async getRelationshipsOfType(type) {
-        const relationships = this.indexes.relationshipType[type] || [];
-        return Promise.all(relationships.map(({src, dest}) => this.get(`${src}-${dest}-${type}`)));
+        const relationshipIds = this.indices.relationshipType.get(type);
+        if (!relationshipIds) {
+            return [];
+        }
+        return await Promise.all(relationshipIds.map(id => this.get(id)));
     }
 
     async getRelationshipsBetweenTypes(type1, type2, relationshipType) {
-        const relationships = this.indexes.relationshipType[relationshipType] || [];
-        let filteredRelationships = [];
-
-        for (let {src, dest} of relationships) {
-            const srcNode = await this.get(src);
-            const destNode = await this.get(dest);
-
-            if (srcNode.data.type === type1 && destNode.data.type === type2) {
-                filteredRelationships.push(await this.get(`${src}-${dest}-${relationshipType}`));
-            }
+        const relationshipIds = this.indices.relationshipType.get(relationshipType);
+        if (!relationshipIds) {
+            return [];
         }
-
-        return filteredRelationships;
+        let relationships = await Promise.all(relationshipIds.map(id => this.get(id)));
+        return relationships.filter(rel => rel.srcId.data.type === type1 && rel.destId.data.type === type2);
     }
 
-   
+    async loadIndices() {
+        const indexFiles = await fs.readdir(this.directory);
+        const indexTypes = Object.keys(this.indices);
+        for (let file of indexFiles) {
+            const [indexType, type] = path.basename(file, '.json').split('/');
+            if (indexTypes.includes(indexType)) {
+                this.indices[indexType].set(type, await this.get(file));
+            }
+        }
+    }
 }
+
