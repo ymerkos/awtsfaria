fe557f90bc41526764aff97d3438bc9d667512f0
Yaakov Kaufer <ykaufer@merkos.local>
2023-08-13 21:18:05 -0400

    BH Extension made

--- geelooy/scripts/tricks/extension/background.js
+++ geelooy/scripts/tricks/extension/background.js
@@ -1,28 +1,53 @@
 //B"H
 // background.js
+console.log("B\"H")
 chrome.runtime.onConnect.addListener(port => {
-    port.onMessage.addListener(message => {
+    port.onMessage.addListener((message) => {
+        console.log("Start",message)
       if (message.action === 'startChatGPT') {
-        // Find or open chat.openai.com tab
+        // ... rest of your code ...
         chrome.tabs.query({ url: 'https://chat.openai.com/*' }, tabs => {
-          if (tabs.length === 0) {
-            chrome.tabs.create({ url: 'https://chat.openai.com/', active: false });
-          }
-        });
+        if (tabs.length === 0) {
+          chrome.tabs.create({ url: 'https://chat.openai.com/', active: false }, tab => {
+            // Inject content script
+            chrome.scripting.executeScript({
+              target: { tabId: tab.id },
+              files: ['content.js']
+            });
+          });
+        } else {
+          // Inject content script into existing tab
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            files: ['content.js']
+          });
+        }
+      });
       }
     });
   });
-  
-  chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
-    if (sender.url.includes('awtsmoos.com') || sender.url.includes('chabad.org')) {
-      if (message.command === 'awtsmoosTseevoy') {
-        chrome.tabs.query({ url: 'https://chat.openai.com/*' }, tabs => {
-          if (tabs.length > 0) {
-            chrome.tabs.sendMessage(tabs[0].id, { command: 'awtsmoosTseevoy', data: message.data }, sendResponse);
-          }
-        });
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    if (message.command === 'awtsmoosTseevoy') {
+        try {
+            chrome.tabs.query({ url: 'https://chat.openai.com/*' }, tabs => {
+            if (tabs.length > 0) {
+                chrome.tabs.sendMessage(tabs[0].id, { command: 'awtsmoosTseevoy', data: message.data }, res => {
+                    console.log("Got response!", res);
+                    sendResponse(res);
+                });
+            }
+            });
+        } catch(e) {
+            sendResponse("Not open")
+        }
         return true; // Indicates async response
       }
-    }
   });
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Got", message)
+    
   
+    
+  });
--- geelooy/scripts/tricks/extension/popup.js
+++ geelooy/scripts/tricks/extension/popup.js
@@ -1,6 +1,37 @@
 //B"H
 // popup.js
+var port = chrome.runtime.connect({ name: 'popup' });
+
 document.getElementById('startButton').addEventListener('click', () => {
-    chrome.runtime.connect().postMessage({ action: 'startChatGPT' });
-  });
-  
+  console.log("got!");
+  port.postMessage({ action: 'startChatGPT' });
+});
+
+document.getElementById('sendCommand').addEventListener('click', () => {
+  let command = document.getElementById('commandInput').value;
+  response.innerHTML ="Maybe loading...";
+  try {
+    chrome.runtime.sendMessage({ command: 'awtsmoosTseevoy', data: command }, res => {
+        console.log("Got res", res);
+        if(!res) {
+            document.getElementById('response').innerHTML = "There was a problem, make sure to START it first!";
+            return;
+        }
+        var r = res;
+        if(r.message) {
+            if(r.message.content.parts[0]) {
+                r = r.message.content.parts[0]
+            }
+        }
+        document.getElementById('response').innerText = r;
+    });
+    } catch(e) {
+        response.innerHTML = "There was an error. Make sure to start ChatGPT first!"
+    }
+});
+
+cp.onclick = async () => {
+    cp.innerHTML = "Copying.."
+    await navigator.clipboard.write(response.innerText);
+    cp.innerHTML = "Copied! Again?"
+}
