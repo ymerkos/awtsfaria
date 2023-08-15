88c259f35705e941cc388731095f4272e71b4b1c
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-08-15 01:25:12 -0400

    B"H
    Better generate id and verify

--- ayzarim/utils.js
+++ ayzarim/utils.js
@@ -1,8 +1,42 @@
 /**
+//B"H
  * @file utils.js
  * Awtsmoos utilities
  */
+const awtsmoosWords = [
+  'Transcendence', 'Enlightenment', 'Emanation', 'Illumination',
+  'Metaphysical', 'Omnipresence', 'Ineffable', 'Seraphic',
+  'Luminosity', 'Resonance', 'Harmonious', 'Celestial',
+  'Ethereal', 'Infinite', 'Divinity', 'Sacrosanct',
+  'Mystical', 'Esoteric', 'Profound', 'Sublime',
+  'Cherubic', 'Cosmic', 'Eternal', 'Majestic',
+  'Revelation', 'Epiphany', 'Awakening', 'Unification',
+  'Existence', 'Creation', 'Omniscience', 'Radiance',
+  'Symphony', 'Convergence', 'Equilibrium', 'Synchronicity',
+    'SacredGeometry', 'UniversalLaw', 'MysticalTruths', 'DivineDance',
+  'InfiniteTapestry', 'CosmicConsciousness', 'SpiritualAlchemy', 'QuantumHarmony',
+  'EternalWisdom', 'MajesticExistence', 'CelestialOrder', 'ProfoundInsight',
+  'AwakeningSoul', 'SacredSymphony', 'MysticJourney', 'CherubicHarmony',
+  'SeraphicSong', 'EnigmaticRevelation', 'CosmicUnity', 'EtherealVision',
+  'MetaphysicalOneness', 'TranscendentLove', 'SublimeEmbrace', 'IneffableBeauty',
+  'DivineResonance', 'CelestialRadiance', 'OmniscientPresence', 'SacrosanctLife',
+  'MysticalOmnipresence', 'LuminousEternity', 'ResonantSymphony', 'HarmoniousEmanation',
+  'CherubicWhispers', 'SeraphicEchoes', 'CosmicReflection', 'EternalDance',
+  'RevelatoryEpiphany', 'AwakeningExistence', 'UnificationSpirit', 'CreationEssence',
+  'OmniscientRadiance', 'TimelessSymphony', 'ConvergenceEquilibrium', 'SynchronicityLife',
+    'HigherWill', 'LowerWill', 'BoundlessEnergy', 'EternalDance',
+  'CosmicResonance', 'CelestialObedience', 'DivineManifestation', 'IneffableMystery',
+  'MysticalOneness', 'UnfathomableDesire', 'SublimeCreation', 'RadiantInfinity',
+  'TimelessWisdom', 'SacredHarmony', 'UniversalResonance', 'CelestialRhythm',
+  'OmnipotentPresence', 'SeraphicGrace', 'TranscendentUnderstanding', 'ProfoundSynchronicity',
+  'IlluminatedInsight', 'OmniscientRevelation', 'LuminousIntuition', 'EssenceOfBeing',
+  'CherubicSymphony', 'MajesticUnity', 'HarmoniousConfluence', 'QuantumVibration',
+  'ImmutableTruth', 'MysticalVoyage', 'SacrosanctWhispers', 'EtherealOmnipresence',
+  'AwtsmoosAwakening', 'DivineEmanation', 'UnfathomableDepth', 'SacredGeometryOfSoul',
+  'AwakeningOfHigherSelf', 'ResonantFrequencyOfCreation', 'InfiniteDanceOfLove', 'SeraphicJourneyIntoAwtsmoos'
 
+  
+];
 class Utils {
     static parseCookies(cookieStr) {
         var ob = cookieStr.split("; ")
@@ -13,6 +47,21 @@ class Utils {
             }, {});
         return ob
     }
+
+    static generateId(name) {
+        if(typeof(name)!="string") name="Awtsmoos";
+        // Select a random word from the collection
+  const randomWord = awtsmoosWords[Math.floor(Math.random() * awtsmoosWords.length)];
+
+  return "BH_" + Date.now() + "_" + randomWord + "_" + name.split(' ')
+    .map((word, index) => {
+      const firstChar = word.charAt(0);
+      const restOfWord = word.slice(1).toLowerCase();
+      return index === 0 ? firstChar.toLowerCase() + restOfWord : firstChar.toUpperCase() + restOfWord;
+    })
+    .join('');
+}
+  
 
      /**
  * Verifies the existence and sanctity of the celestial characters.
