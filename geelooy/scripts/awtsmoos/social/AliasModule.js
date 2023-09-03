669bc558093eeda797932ce19bd9f83d952c756b
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-09-03 16:25:43 -0400

    B"H
    Create AliasModule.js

---
+++ geelooy/scripts/awtsmoos/social/AliasModule.js
@@ -0,0 +1,20 @@
+/**
+B"H
+for displaying and editing one's aliases.
+@requires div with id aliasList before this script loads
+as well as AwtsmoosSocialHandler 
+**/
+import AwtsmoosSocialHandler from './AwtsmoosSocialHandler.js';
+
+const aliasesHandler = new AwtsmoosSocialHandler('/api');
+
+aliasesHandler.fetchEntities('/aliases')
+  .then(data => {
+    aliasesHandler.displayEntities(data, 'aliasList', (entity, elem) => {
+      elem.contentEditable = true;
+      elem.addEventListener('blur', function() {
+        aliasesHandler.editEntity(entity.aliasId, { aliasName: this.textContent }, '/aliases');
+        this.contentEditable = false;
+      });
+    });
+  });
