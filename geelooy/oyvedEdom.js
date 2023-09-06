3e28006c2b748550ccfebf6e619e173fc7601216
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-06 02:23:02 -0400

    BH service worker properly handled

--- geelooy/oyvedEdom.js
+++ geelooy/oyvedEdom.js
@@ -2,16 +2,61 @@
  * B"H
  * service worker
  */
-
+var cacheName = "awtsmoosCash";
+console.log("Service")
 //caching
 self.addEventListener('fetch', (event) => {
+    console.log("fetching", event)
     event.respondWith(
         caches.match(event.request)
         .then((cachedResponse) => {
             if (cachedResponse) {
+            //    console.log("Got cache",cachedResponse)
                 return cachedResponse;
             }
-            return fetch(event.request);
+            
+            // IMPORTANT: Clone the request object
+            // A request is a stream and can only be consumed once
+            const fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then((response) => {
+                // Check if the received response is valid
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    //console.log("basic response", response)
+                    return response;
+                }
+
+                // IMPORTANT: Clone the response object
+                // A response is a stream and because we want the browser
+                // to consume the response as well as the cache to consume
+                // the response, we need to clone it so we have two streams.
+                const responseToCache = response.clone();
+
+                // Cache the response
+                caches.open(cacheName)
+                .then((cache) => {
+                    cache.put(event.request, responseToCache);
+                 //   console.log("Cached!",cacheName)
+                });
+
+                return response;
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+   // console.log("active",event)
+    clients.claim();
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+        return Promise.all(
+            cacheNames.map((cache) => {
+            if (cache !== cacheName) {
+                return caches.delete(cache);
+            }
+            })
+        );
         })
     );
 });
