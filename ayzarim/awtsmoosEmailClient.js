89878698d9a3d9834bd86d8f4119ce8bf73bc5df
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-21 21:34:52 -0400

    BH secure sending

--- ayzarim/awtsmoosEmailClient.js
+++ ayzarim/awtsmoosEmailClient.js
@@ -12,87 +12,128 @@
     );
 
  */
-const net = require('net');
-const CRLF = '\r\n';
-class AwtsmoosEmailClient {
-    constructor(smtpServer, port = 25) {
-        this.smtpServer = smtpServer;
-        this.port = port;
-    }
-
-    sendMail(sender, recipient, data) {
-        const client = net.createConnection(this.port, this.smtpServer, () => {
-            console.log('Connected to SMTP server, to send');
-        });
-
-        client.setEncoding('utf-8');
-
-        let stage = 0;
-
-        client.on('data', (data) => {
-            console.log('Server: ' + data);
-
-            // Check for error response from server
-            if (data.startsWith('4') || data.startsWith('5')) {
-                console.error('Error from server:', data);
-                client.end();
-                return;
-            }
-
-            switch (stage) {
-                case 0:
-                    client.write(`EHLO awtsmoos.one${CRLF}`);
-                    
-                    stage++;
-                    console.log("EHLO command", stage)
-                    break;
-                case 1:
-                    client.write(`MAIL FROM:<${sender}>${CRLF}`);
-                    stage++;
-                    console.log("MF command", stage)
-                    break;
-                case 2:
-                    client.write(`RCPT TO:<${recipient}>${CRLF}`);
-                    stage++;
-                    console.log("RC command", stage)
-                    break;
-                case 3:
-                    client.write(`DATA${CRLF}`);
-                    stage++;
-                    console.log("data command", stage)
-                    break;
-                case 4:
-                    client.write(`${data}${CRLF}.${CRLF}`);
-                    stage++;
-                    console.log("data write", stage)
-                    break;
-                case 5:
-                    client.write(`QUIT${CRLF}`);
-                    stage++;
-                    console.log("quitted", stage)
-                    break;
-                default:
+    const net = require('net');
+    const tls = require('tls');
+    const CRLF = '\r\n';
+    
+    class AwtsmoosEmailClient {
+        constructor(smtpServer, port = 587, username, password) {
+            this.smtpServer = smtpServer;
+            this.port = port;
+            this.username = Buffer.from(username).toString('base64');
+            this.password = Buffer.from(password).toString('base64');
+        }
+    
+        sendMail(sender, recipient, data) {
+            const client = net.createConnection(this.port, this.smtpServer, () => {
+                console.log('Connected to SMTP server');
+            });
+    
+            client.setEncoding('utf-8');
+            let stage = 0;
+    
+            client.on('data', (data) => {
+                console.log('Server: ' + data);
+    
+                if (data.startsWith('4') || data.startsWith('5')) {
+                    console.error('Error from server:', data);
                     client.end();
-                    console.log("ended", stage)
-                    break;
-            }
-        });
-
-        client.on('end', () => {
-            console.log('Connection closed');
-        });
-
-        client.on('error', (err) => {
-            console.error('Error:', err);
-        });
+                    return;
+                }
+    
+                switch (stage) {
+                    case 0:
+                        client.write(`EHLO localhost${CRLF}`);
+                        stage++;
+                        break;
+                    case 1:
+                        if (data.includes('STARTTLS')) {
+                            client.write(`STARTTLS${CRLF}`);
+                            stage++;
+                        } else {
+                            console.error('Server does not support STARTTLS');
+                            client.end();
+                        }
+                        break;
+                    case 2:
+                        const secureClient = tls.connect({
+                            socket: client,
+                            rejectUnauthorized: false
+                        }, () => {
+                            secureClient.write(`AUTH LOGIN${CRLF}`);
+                            stage++;
+                        });
+    
+                        secureClient.setEncoding('utf-8');
+    
+                        secureClient.on('data', (secureData) => {
+                            console.log('Secure Server:', secureData);
+    
+                            switch (stage) {
+                                case 3:
+                                    secureClient.write(`${this.username}${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 4:
+                                    secureClient.write(`${this.password}${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 5:
+                                    secureClient.write(`MAIL FROM:<${sender}>${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 6:
+                                    secureClient.write(`RCPT TO:<${recipient}>${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 7:
+                                    secureClient.write(`DATA${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 8:
+                                    secureClient.write(`${data}${CRLF}.${CRLF}`);
+                                    stage++;
+                                    break;
+                                case 9:
+                                    secureClient.write(`QUIT${CRLF}`);
+                                    stage++;
+                                    break;
+                                default:
+                                    secureClient.end();
+                                    break;
+                            }
+                        });
+    
+                        secureClient.on('end', () => {
+                            console.log('Secure Connection closed');
+                        });
+    
+                        secureClient.on('error', (err) => {
+                            console.error('Secure Error:', err);
+                        });
+                        break;
+                }
+            });
+    
+            client.on('end', () => {
+                console.log('Connection closed');
+            });
+    
+            client.on('error', (err) => {
+                console.error('Error:', err);
+            });
+        }
     }
-}
-
-const smtpClient = new AwtsmoosEmailClient('awtsmoos.one');
-console.log("Testing server")
-smtpClient.sendMail(
-    'me@awtsmoos.one',
-    'cobykaufer@gmail.com',
-    'Subject: Test email\r\n\r\nB"H '
-);
+    
+    const username = process.env.username;
+    const password = process.env.password;
+    const smtpClient = new AwtsmoosEmailClient(
+        'smtp.gmail.com', 587, username, password
+    );
+    smtpClient.sendMail(
+        'me@awtsmoos.one', 
+        'cobykaufer@gmail.com', 
+        'Subject: Test email\r\n\r\nThis is a test email.'
+    );
+    
 module.exports = AwtsmoosEmailClient;
