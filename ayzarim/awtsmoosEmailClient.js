4adf682b82c34752874d82795d5a6a785d0b3758
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-22 02:03:09 -0400

    BH

--- ayzarim/awtsmoosEmailClient.js
+++ ayzarim/awtsmoosEmailClient.js
@@ -90,32 +90,37 @@ class AwtsmoosEmailClient {
     handleSMTPResponse(line, client, sender, recipient, emailData) {
         console.log('Server Response:', line);
 
+        // Check for error responses before proceeding
         this.handleErrorCode(line);
 
+        this.multiLineResponse += line + CRLF;
         if (line.endsWith('-')) {
-            console.log('Multi-line Response:', line);
+            console.log('Multi-line Response:', this.multiLineResponse);
             return;
         }
 
+        // Execute next command or end connection based on response
         this.previousCommand = this.currentCommand;
-        
-        // Check if this.currentCommand is END OF DATA
         if (this.currentCommand === 'END OF DATA') {
-            client.end(); // End the client connection after receiving a successful response for END OF DATA
+            client.end();
             return;
         }
         
         const nextCommand = this.getNextCommand();
-        
         const commandHandlers = {
             'EHLO': () => client.write(`MAIL FROM:<${sender}>${CRLF}`),
             'MAIL FROM': () => client.write(`RCPT TO:<${recipient}>${CRLF}`),
             'RCPT TO': () => client.write(`DATA${CRLF}`),
             'DATA': () => client.write(`${emailData}${CRLF}.${CRLF}`),
-            'END OF DATA': () => client.end(),
         };
 
         const handler = commandHandlers[nextCommand];
+     
+        handler();
+        this.currentCommand = nextCommand;
+        this.multiLineResponse = ''; // Clear multiLineResponse for the next command
+    
+        
 
         if (!handler) {
             throw new Error(`Unknown next command: ${nextCommand}`);
@@ -123,6 +128,8 @@ class AwtsmoosEmailClient {
 
         handler();
         this.currentCommand = nextCommand;
+        this.multiLineResponse = ''; // Clear multiLineResponse for the next command
+    
     }
 
     /**
