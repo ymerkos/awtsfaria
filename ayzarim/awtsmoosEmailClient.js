dd864f6683c90175d62e885f4e95867d8436d7f2
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-22 00:42:50 -0400

    BH multiline

--- ayzarim/awtsmoosEmailClient.js
+++ ayzarim/awtsmoosEmailClient.js
@@ -4,48 +4,44 @@
  * a client for SENDING emails
  * @requires crypto
  */
-const crypto = require("crypto");
+
+const crypto = require('crypto');
 const net = require('net');
+const tls = require('tls');
+
 const CRLF = '\r\n';
 
 class AwtsmoosEmailClient {
-    constructor(smtpServer, port = 25, username = null, password = null, privateKey = null) {
+    constructor(smtpServer, port = 25, privateKey = null) {
         this.smtpServer = smtpServer;
         this.port = port;
-        this.username = username;
-        this.password = password;
-        // Normalize the private key
+
         this.privateKey = privateKey ? privateKey.replace(/\\n/g, '\n') : null;
     }
 
     canonicalizeRelaxed(headers, body) {
-        // Canonicalize headers and body in relaxed mode
         const canonicalizedHeaders = headers.split(CRLF)
             .map(line => line.toLowerCase().split(/\s*:\s*/).join(':').trim())
             .join(CRLF);
-        
+
         const canonicalizedBody = body.split(CRLF)
             .map(line => line.split(/\s+/).join(' ').trimEnd())
             .join(CRLF).trimEnd();
-        
+
         return { canonicalizedHeaders, canonicalizedBody };
     }
 
     signEmail(domain, selector, privateKey, emailData) {
-        // Extract headers and body
         const [headers, ...bodyParts] = emailData.split(CRLF + CRLF);
         const body = bodyParts.join(CRLF + CRLF);
 
-        // Canonicalize and create body hash
         const { canonicalizedHeaders, canonicalizedBody } = this.canonicalizeRelaxed(headers, body);
         const bodyHash = crypto.createHash('sha256').update(canonicalizedBody).digest('base64');
 
-        // Construct DKIM header and sign it
         const dkimHeader = `v=1;a=rsa-sha256;c=relaxed/relaxed;d=${domain};s=${selector};bh=${bodyHash};h=from:to:subject:date;`;
         const signature = crypto.createSign('SHA256').update(dkimHeader + canonicalizedHeaders).sign(privateKey, 'base64');
-        const formattedSignature = signature.match(/.{1,72}/g).join(CRLF);
-        
-        return `${dkimHeader}b=${formattedSignature}`;
+
+        return `${dkimHeader}b=${signature}`;
     }
 
     handleSMTPResponse(line, client, sender, recipient, emailData) {
@@ -55,18 +51,13 @@ class AwtsmoosEmailClient {
         } else if (line.startsWith('220 ')) {
             client.write(`EHLO ${this.smtpServer}${CRLF}`);
         } else if (line.startsWith('250 ')) {
-            if (this.username && this.password) {
-                const auth = Buffer.from(`${this.username}:${this.password}`).toString('base64');
-                client.write(`AUTH LOGIN ${auth}${CRLF}`);
-            } else {
-                client.write(`MAIL FROM:<${sender}>${CRLF}`);
-            }
+            client.write(`MAIL FROM:<${sender}>${CRLF}`);
         } else if (line.startsWith('235 ') || line.startsWith('250 2.1.0')) {
             client.write(`RCPT TO:<${recipient}>${CRLF}`);
         } else if (line.startsWith('250 2.1.5')) {
             client.write(`DATA${CRLF}`);
         } else if (line.startsWith('354')) {
-            const domain = this.username.split('@')[1];
+            const domain = sender.split('@')[1];
             const selector = 'default';
             const dkimSignature = this.signEmail(domain, selector, this.privateKey, emailData);
             client.write(`DKIM-Signature: ${dkimSignature}${CRLF}${emailData}${CRLF}.${CRLF}`);
@@ -79,18 +70,18 @@ class AwtsmoosEmailClient {
     }
 
     async sendMail(sender, recipient, subject, body) {
-        const client = net.createConnection(this.port, this.smtpServer);
-        client.setEncoding('utf-8');
-        let buffer = '';
-        
-        const emailData = `From: ${sender}${CRLF}To: ${recipient}${CRLF}Subject: ${subject}${CRLF}${CRLF}${body}`;
-
         return new Promise((resolve, reject) => {
-            const secureClient = tls.connect({ host: this.smtpServer, port: 587 }, () => {
-                secureClient.write(`EHLO ${this.smtpServer}${CRLF}`);
+            const client = net.createConnection(this.port, this.smtpServer);
+            client.setEncoding('utf-8');
+            let buffer = '';
+
+            const emailData = `From: ${sender}${CRLF}To: ${recipient}${CRLF}Subject: ${subject}${CRLF}${CRLF}${body}`;
+
+            client.on('connect', () => {
+                client.write(`EHLO ${this.smtpServer}${CRLF}`);
             });
 
-            secureClient.on('data', (data) => {
+            client.on('data', (data) => {
                 buffer += data;
                 let index;
                 while ((index = buffer.indexOf(CRLF)) !== -1) {
@@ -99,32 +90,38 @@ class AwtsmoosEmailClient {
 
                     try {
                         if (!line.endsWith('-')) {
-                            this.handleSMTPResponse(line, secureClient, sender, recipient, emailData);
+                            this.handleSMTPResponse(line, client, sender, recipient, emailData);
                         }
                     } catch (err) {
                         reject(err);
+                        return;
                     }
                 }
             });
 
-            secureClient.on('end', resolve);
-            secureClient.on('error', (err) => {
-                console.error('Connection Error:', err);
-                reject(err);
-            });
+            client.on('end', resolve);
+            client.on('error', reject);
         });
     }
 }
 
-// Extract credentials from environment variables
-const username = process.env.username;
-const password = process.env.password;
+
 const privateKey = process.env.BH_key;
 
-// Create SMTP client instance and send email
-const smtpClient = new AwtsmoosEmailClient('awtsmoos.one', 25, username, password, privateKey);
-smtpClient.sendMail('me@awtsmoos.one', 'awtsmoos@gmail.com', 'Test email', 'This is a test email.')
-    .then(() => console.log('Email sent successfully'))
-    .catch(err => console.error('Failed to send email:', err));
+const smtpClient = new AwtsmoosEmailClient('smtp.yourserver.com', 25, privateKey);
+
+async function main() {
+    try {
+        await smtpClient.sendMail(
+            'me@awtsmoos.com', 'awtsmoos@gmail.com', 
+            'B"H', 'This is a test email.'
+        );
+        console.log('Email sent successfully');
+    } catch (err) {
+        console.error('Failed to send email:', err);
+    }
+}
+
+main();
 
 module.exports = AwtsmoosEmailClient;
