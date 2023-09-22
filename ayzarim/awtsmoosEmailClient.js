ac43ea8ea82e7fdf53decc8b81297e4e43e725c9
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-21 21:09:32 -0400

    BH email client fix other newline fixes

--- ayzarim/awtsmoosEmailClient.js
+++ ayzarim/awtsmoosEmailClient.js
@@ -12,63 +12,77 @@
     );
 
  */
-
 const net = require('net');
 const CRLF = '\r\n';
 
 class AwtsmoosEmailClient {
-  constructor(smtpServer, port = 25) {
-    this.smtpServer = smtpServer;
-    this.port = port;
-  }
+    constructor(smtpServer, port = 25) {
+        this.smtpServer = smtpServer;
+        this.port = port;
+    }
 
-  sendMail(sender, recipient, data) {
-    const client = net.createConnection(this.port, this.smtpServer, () => {
-      console.log('Connected to SMTP server');
-    });
+    sendMail(sender, recipient, data) {
+        const client = net.createConnection(this.port, this.smtpServer, () => {
+            console.log('Connected to SMTP server, to send');
+        });
 
-    client.setEncoding('utf-8');
+        client.setEncoding('utf-8');
 
-    let stage = 0;
+        let stage = 0;
 
-    client.on('data', (data) => {
-      console.log('Server: ' + data);
+        client.on('data', (data) => {
+            console.log('Server: ' + data);
 
-      switch (stage) {
-        case 0:
-          client.write(`EHLO localhost${CRLF}`);
-          stage++;
-          break;
-        case 1:
-          client.write(`MAIL FROM:<${sender}>${CRLF}`);
-          stage++;
-          break;
-        case 2:
-          client.write(`RCPT TO:<${recipient}>${CRLF}`);
-          stage++;
-          break;
-        case 3:
-          client.write(`DATA${CRLF}`);
-          stage++;
-          break;
-        case 4:
-          client.write(`${data}${CRLF}.${CRLF}`);
-          stage++;
-          break;
-        case 5:
-          client.write(`QUIT${CRLF}`);
-          stage++;
-          break;
-      }
-    });
+            // Check for error response from server
+            if (data.startsWith('4') || data.startsWith('5')) {
+                console.error('Error from server:', data);
+                client.end();
+                return;
+            }
 
-    client.on('end', () => {
-      console.log('Disconnected from SMTP server');
-    });
+            switch (stage) {
+                case 0:
+                    client.write(`EHLO awtsmoos.one${CRLF}`);
+                    stage++;
+                    console.log("EHLO. Sending state is ",stage)
+                    break;
+                case 1:
+                    client.write(`MAIL FROM:<${sender}>${CRLF}`);
+                    stage++;
+                    
+                    console.log("MAIL FROM:. Sending state is ",stage)
+                    break;
+                case 2:
+                    client.write(`RCPT TO:<${recipient}>${CRLF}`);
+                    stage++;
+                    console.log("RCPT TO:. Sending state is ",stage)
+                    break;
+                case 3:
+                    client.write(`DATA${CRLF}`);
+                    stage++;
+                    console.log("new line:. Sending state is ",stage)
+                    break;
+                case 4:
+                    client.write(`${data}${CRLF}.${CRLF}`);
+                    stage++;
+                    console.log("writing data:. Sending state is ",stage)
+                    break;
+                case 5:
+                    client.write(`QUIT${CRLF}`);
+                    stage++;
+                    console.log("quit sending:. Sending state is ",stage)
+                    break;
+            }
+        });
 
-    client.on('error', (err) => {
-      console.log('Error:', err);
-    });
-  }
+        client.on('end', () => {
+            console.log('Disconnected from SMTP server');
+        });
+
+        client.on('error', (err) => {
+            console.log('Error:', err);
+        });
+    }
 }
+
 module.exports = AwtsmoosEmailClient;
--- ayzarim/email.js
+++ ayzarim/email.js
@@ -1,10 +1,9 @@
-
 /**
  * B"H
  * @module AwtsMail
  */
 
-const AwtsmoosClient = require("./awtsmoosEmailClient.js")
+const AwtsmoosClient = require("./awtsmoosEmailClient.js");
 const net = require('net');
 const CRLF = '\r\n';
 
@@ -12,76 +11,72 @@ module.exports = class AwtsMail {
     constructor() {
         console.log("Starting instance of email");
 
-        console.log("Sending test email")
-       
-
         this.server = net.createServer(socket => {
             console.log("Some connection happened!", Date.now());
             socket.write('220 awtsmoos.one ESMTP Postfix' + CRLF);
-            
+
             let sender = '';
             let recipients = [];
             let data = '';
             let receivingData = false;
-            
+
             socket.on('data', chunk => {
-                const command = chunk.toString().trim();
+                const commands = chunk.toString().split(CRLF);
 
-                console.log("Received command:", command);
-                console.log("Command length:", command.length);
+                for (const command of commands) {
+                    console.log("Received command:", command);
+                    console.log("Command length:", command.length);
 
-                if (receivingData) {
-                    data += command + CRLF;
-                    console.log("Current command: ",command)
-                    if (command === '.') {
-                        receivingData = false;
-                        console.log("Received email data:", data);
-                        socket.write(`250 2.0.0 Ok: queued as 12345${CRLF}`);
+                    if (receivingData) {
+                        if (command === '.') {
+                            receivingData = false;
+                            console.log("Received email data:", data);
 
+                            socket.write(`250 2.0.0 Ok: queued as 12345${CRLF}`);
 
-                        // Simulate sending a reply back.
-                        if (sender) {
-                          console.log(`Sending a reply back to ${sender}`);
-                          const replyData = `Subject: Reply from Awtsmoos ${
-                            Math.floor(Math.random()*8)
-                          }\r\n\r\nB"H\n\nHello from the Awtsmoos, the time is ${
-                            Date.now()
-                          }.`;
-                          this
-                          .smtpClient
-                          .sendMail('reply@awtsmoos.one', sender, replyData);
+                            // Simulate sending a reply back.
+                            if (sender) {
+                                console.log(`Sending a reply back to ${sender}`);
+                                const replyData = `Subject: Reply from Awtsmoos ${
+                                  Math.floor(Math.random() * 8)
+                                }\r\n\r\nB"H\n\nHello from the Awtsmoos, the time is ${
+                                  Date.now()
+                                }.`;
+                                this.smtpClient.sendMail('reply@awtsmoos.one', sender, replyData);
+                            }
+                        } else {
+                            data += command + CRLF;
                         }
+                        continue;
                     }
-                    return;
-                }
-                
-                if (command.startsWith('EHLO') || command.startsWith('HELO')) {
-                    socket.write(`250-Hello${CRLF}`);
-                    socket.write(`250 SMTPUTF8${CRLF}`);
-                } else if (command.startsWith('MAIL FROM')) {
-                    sender = command.slice(10);
-                    socket.write(`250 2.1.0 Ok${CRLF}`);
-                    console.log("The SENDER is:", sender);
 
-                } else if (command.startsWith('RCPT TO')) {
-                    recipients.push(command.slice(8));
-                    socket.write(`250 2.1.5 Ok${CRLF}`);
-                } else if (command.startsWith('DATA')) {
-                    receivingData = true;
-                    socket.write(`354 End data with <CR><LF>.<CR><LF>${CRLF}`);
-                } else if (command.startsWith('QUIT')) {
-                    socket.write(`221 2.0.0 Bye${CRLF}`);
-                    socket.end();
-                } else {
-                    console.log("Unknown command:", command);
-                    socket.write('500 5.5.1 Error: unknown command' + CRLF);
+                    if (command.startsWith('EHLO') || command.startsWith('HELO')) {
+                        socket.write(`250-Hello${CRLF}`);
+                        socket.write(`250 SMTPUTF8${CRLF}`);
+                    } else if (command.startsWith('MAIL FROM')) {
+                        sender = command.slice(10);
+                        socket.write(`250 2.1.0 Ok${CRLF}`);
+                        console.log("The SENDER is:", sender);
+                    } else if (command.startsWith('RCPT TO')) {
+                        recipients.push(command.slice(8));
+                        socket.write(`250 2.1.5 Ok${CRLF}`);
+                    } else if (command.startsWith('DATA')) {
+                        receivingData = true;
+                        socket.write(`354 End data with <CR><LF>.<CR><LF>${CRLF}`);
+                    } else if (command.startsWith('QUIT')) {
+                        socket.write(`221 2.0.0 Bye${CRLF}`);
+                        socket.end();
+                    } else {
+                        console.log("Unknown command:", command);
+                        socket.write('500 5.5.1 Error: unknown command' + CRLF);
+                    }
                 }
             });
-            
+
             socket.on("error", err => {
                 console.log("Socket error:", err);
             });
-            
+
             socket.on("close", () => {
                 console.log("Connection closed");
             });
@@ -89,23 +84,11 @@ module.exports = class AwtsMail {
 
         this.smtpClient = new AwtsmoosClient("awtsmoos.one");
 
-/*
-
-        const email = `Subject: Email from Awtsmoos ${
-          Math.floor(Math.random()*8)
-        } to the Awts.\r\n\r\nB"H\n\nHello from the Awtsmoos, the time is ${
-          Date.now()
-        }.`;
-        this
-        .smtpClient
-        .sendMail('essence@awtsmoos.one', "cobykaufer@gmail.com", email);
-*/
-
         this.server.on("error", err => {
             console.log("Server error: ", err);
         });
     }
-    
+
     shoymayuh() {
         this.server.listen(25, () => {
             console.log("Awtsmoos mail listening to you, port 25");
