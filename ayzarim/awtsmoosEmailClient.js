3a7d63ccf76cbac4e712d3e46d63f76ff218372b
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-27 01:15:40 -0400

    BH more handlers

--- ayzarim/awtsmoosEmailClient.js
+++ ayzarim/awtsmoosEmailClient.js
@@ -101,7 +101,8 @@ class AwtsmoosEmailClient {
             sender,
             recipient,
             emailData,
-            client
+            client,
+            lineOrMultiline 
         } = {}) => {
             // Read the response from the server
             
@@ -158,7 +159,8 @@ class AwtsmoosEmailClient {
                     sender,
                     recipient,
                     emailData,
-                    client
+                    client,
+                    lineOrMultiline
                 });
             });
     
