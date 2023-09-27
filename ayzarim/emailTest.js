d90670b30f5367a38af6209253d0a3ac80bf2223
Yaakov Kaufer <ykaufer@merkos.local>
2023-09-27 00:08:59 -0400

    BH more test file

--- ayzarim/emailTest.js
+++ ayzarim/emailTest.js
@@ -2,7 +2,6 @@
  * B"H
  * a script used for email testing
  */
-
 const net = require('net');
 const tls = require('tls');
 
@@ -14,32 +13,39 @@ const socket = net.connect(25, 'gmail-smtp-in.l.google.com', () => {
   socket.write('EHLO localhost\r\n');
 });
 
+let response = '';
+
 socket.on('data', (data) => {
+  response += data.toString();
   console.log(data.toString());
-  
-  // If the server responds with 250, it is ready to start TLS
-  if (data.toString().startsWith('250 ')) {
-    console.log('Starting TLS...');
-    
-    // Upgrade the socket to a secure TLS socket
-    const secureSocket = tls.connect({
-      socket: socket,
-      servername: 'gmail-smtp-in.l.google.com',
-    }, () => {
-      console.log('TLS connection established');
-      secureSocket.end();
-    });
 
-    secureSocket.on('data', (data) => {
-      console.log(data.toString());
-    });
+  // Check if the last line in the response is the end of the EHLO response
+  if (response.split('\n').some(line => line.startsWith('250 '))) {
+    // Check if the server is ready to start TLS
+    if (response.includes('250-STARTTLS')) {
+      console.log('Starting TLS...');
+      
+      // Upgrade the socket to a secure TLS socket
+      const secureSocket = tls.connect({
+        socket: socket,
+        servername: 'gmail-smtp-in.l.google.com',
+      }, () => {
+        console.log('TLS connection established');
+        secureSocket.end();
+      });
 
-    secureSocket.on('error', (err) => {
-      console.error('TLS error:', err);
-    });
+      secureSocket.on('data', (data) => {
+        console.log(data.toString());
+      });
+
+      secureSocket.on('error', (err) => {
+        console.error('TLS error:', err);
+      });
+    }
   }
 });
 
 socket.on('error', (err) => {
   console.error('Socket error:', err);
 });
+
