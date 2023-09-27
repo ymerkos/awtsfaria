29548cd466cf2e409a2933af54d7bfdf86f802af
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-09-27 14:20:34 -0400

    B”H

---
+++ geelooy/scripts/awtsmoos/social/PostsModal.js
@@ -0,0 +1,57 @@
+/**
+B"H
+@requires div with id "heichelList"
+
+for displaying and editing one's heichels.
+@requires div with id aliasList before this script loads
+as well as AwtsmoosSocialHandler 
+**/
+
+import EntityModule from './EntityModule.js';
+
+const heichelsHandler = new EntityModule({
+  apiEndpoint:'/api/social/',
+  containerID:"postList",
+  entityIds: "postIds",
+  entityType:"posts", //entityType
+  editableFields: [
+    "postId",
+    "title",
+    "content"
+  ],
+  readonlyFields: [
+    "author"
+  ],
+  getFn: async (entity, mod) => {
+    
+    var heych = await mod.handler.getHeichel(entity)
+    
+    return heych
+  },
+  updateDataFn: async (r) => {
+    console.log(r)
+    return {
+      aliasId: r.entity.author,
+      postId: r.id,
+      content:r.updatedData. content || 
+        r.entity. content,
+      title: r.updatedData. title|| r.entity. title
+    }
+  },
+  createFn: async m => {
+    var heichelName = prompt("enter post title")
+    var aliasID = prompt ("Enter your alias ID to match it")
+    var description = prompt("enter content")
+    var r = await m.handler.createEntity({
+      entityType: "posts",
+      newEntityData: {
+        title: heichelName,
+        content: description,
+        aliasId: aliasID,
+        isPublic: true
+      }
+    })
+    console.log("MAde! heichel", r)
+  }
+}
+);
