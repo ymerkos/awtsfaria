9adb884c109c8fe6881ed12e9008da6426327728
ymerkos <ykaufer@merkos302.com>
2023-11-05 19:14:32 -0500

    BH binary search

--- geelooy/sefaria/templates/viewer.js
+++ geelooy/sefaria/templates/viewer.js
@@ -75,38 +75,93 @@ function parseData(inputHTML) {
 }
  const container = document.querySelector('.intensityAwtsmoos'); // This is the scroll container
  window.cin=container
+ const paragraphPositions = [];
+ window.pp = paragraphPositions
 function observeStuff() {
    
-    let lastKnownScrollPosition = 0;
-   
+    const paragraphs = container.querySelectorAll('.paragraph');
+	
 
-    // Function to highlight the topmost fully visible paragraph
-    function highlightTopParagraph() {
-		console.log("YO!")
-		
-        let highlighted = false;
-        container.querySelectorAll('.paragraph, .paragraph-selected').forEach(para => {
-            const paraTop = para.offsetTop - container.scrollTop;
-            const paraBottom = paraTop + para.clientHeight;
-			
-            // Check if the paragraph is fully within the visible area of the container
-            if (paraTop >= 0 && paraBottom <= container.clientHeight && !highlighted) {
-				console.log(paraTop,container.scrollTop,para)	
-                para.classList.add('paragraph-selected');
-				para.classList.remove('paragraph')
-                highlighted = true;
-            } else {
-				para.classList.add('paragraph')
-                para.classList.remove('paragraph-selected');
-            }
-        });
-    }
+	// Cache positions and heights of paragraphs
+	function cacheParagraphPositions() {
+		paragraphPositions.length = 0; // Clear any existing entries
+		paragraphs.forEach((para) => {
+			const rect = para.getBoundingClientRect();
+			// Cache the absolute top offset, not the relative one
+			const absoluteTop = rect.top + window.pageYOffset;
+			paragraphPositions.push({
+				element: para,
+				top: absoluteTop,
+				bottom: absoluteTop + rect.height
+			});
+		});
+		highlightCurrentParagraph()
+	}
 
-    // Initial highlight when content is loaded
-    highlightTopParagraph();
+	// Run this function when the page loads or when paragraphs are generated
+	cacheParagraphPositions();
+	
+	
+	// Function to perform a binary search on the array of paragraph positions
+	function findCurrentParagraph(scrollPosition, offset = 200) {
+	  let start = 0;
+	  let end = paragraphPositions.length - 1;
 
-    // Highlight top paragraph on scroll
-    container.addEventListener('scroll', highlightTopParagraph);
+	  while (start <= end) {
+		const mid = Math.floor((start + end) / 2);
+		const para = paragraphPositions[mid];
+
+		if (scrollPosition + offset >= para.top && scrollPosition + offset < para.bottom) {
+		  return para.element;
+		} else if (scrollPosition + offset < para.top) {
+		  end = mid - 1;
+		} else {
+		  start = mid + 1;
+		}
+	  }
+	  return null;
+	}
+
+	// Function to highlight the current paragraph based on the binary search result
+	function highlightCurrentParagraph() {
+	  const scrollPosition = container.scrollTop + container.offsetTop;
+	  const currentParagraph = findCurrentParagraph(scrollPosition);
+
+	  // If a current paragraph is found and it's not already highlighted
+	  if (currentParagraph && !currentParagraph.classList.contains('paragraph-selected')) {
+		// Remove the class from the previously selected paragraph if any
+		const previousSelected = container.querySelector('.paragraph-selected');
+		if (previousSelected) {
+		  previousSelected.classList.remove('paragraph-selected');
+		}
+
+		// Add the class to the current paragraph
+		currentParagraph.classList.add('paragraph-selected');
+	  }
+	}
+
+
+	// Highlight top paragraph on scroll
+	container.addEventListener('scroll', highlightCurrentParagraph);
+	
+	// You can also run cacheParagraphPositions when the window is resized to update positions
+	// as they might change if the layout is responsive
+	function debounce(func, wait) {
+	  let timeout;
+	  return function executedFunction(...args) {
+		const later = () => {
+		  clearTimeout(timeout);
+		  func(...args);
+		};
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	  };
+	};
+
+	// Wrap the resize listener call in the debounce function
+	window.addEventListener('resize', debounce(() => {
+	  cacheParagraphPositions();
+	}, 500)); // Waits for 250ms of no resize events before executing
 
 
 }
