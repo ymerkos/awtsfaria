8eff60108cc4c7cd72d2ab0e857499e6a83ac931
ymerkos <121984148+ymerkos@users.noreply.github.com>
2023-11-12 16:38:02 -0500

    B"H

--- geelooy/scripts/awtsmoos/social/HeichelosManager.js
+++ geelooy/scripts/awtsmoos/social/HeichelosManager.js
@@ -25,9 +25,10 @@ async function go(myAlias) {
         <a target="_blank" href="/u/heichelos/${
           entity.id
         }/posts">View and edit posts for Heichel</a>
+        <br>
         <a target="_blank" href="/heichelos/${
           entity.id
-        }/?editing=${myAlias}">View and edit Heichel in detail <br>
+        }/?editingAlias=${myAlias}">View and edit Heichel in detail <br>
         including posts and series and order</a>
       
       `
