ef8fddd685c586848f969b58e4feb2b887a2f553
Yaakov Kaufer <ykaufer@merkos.local>
2023-11-29 00:34:47 -0500

    BH

--- geelooy/api/social/helper/counter.js
+++ geelooy/api/social/helper/counter.js
@@ -41,12 +41,12 @@ async function increaseCount({
         return c;
     }
     var site = c.title;
-    try {
+    /*/*try {
     if(typeof(site) == "string" && site.length)
     $i.setHeader(
         "Access-Control-Allow-Origin",
         "*"//TODO make it dependenat on "site"
-    );
+    );/
     } catch(e) {
         console.log("Tried to header but no: ",e)
     }
