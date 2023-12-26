be9e8cb85a804f31d1ec42fd66e8b8ffbd375f71
Yaakov Kaufer <ykaufer@merkos.local>
2023-12-25 19:11:29 -0500

    BH

--- geelooy/scripts/awtsmoosTamper.js
+++ geelooy/scripts/awtsmoosTamper.js
@@ -604,7 +604,7 @@ function syntaxHighlight(contentEditableElement, mode) {
 			words = ["abstract", "arguments", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete",
 				"do", "double", "else", "enum", "eval", "export", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import",
 				"in", "instanceof", "int", "interface", "let", "long", "NaN", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static",
-				"super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while", "with", "yield"
+				"super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "volatile", "while", "with", "yield", "async", "await"
 			];
 		}
 		for (i = 0; i < words.length; i++) {
