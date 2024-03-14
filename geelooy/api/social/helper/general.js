a475ee33881bac2ab5b844d1e969a9d933578004
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-03-14 16:40:26 -0400

    B"H

--- geelooy/api/social/helper/general.js
+++ geelooy/api/social/helper/general.js
@@ -11,11 +11,16 @@ module.exports = {
 function myOpts($i){
 	var maxOrech=$i.$_GET.maxOrech ||
 			$i.$_GET.maxLength;
+	try {
+		if(maxOrech) {
 	var num=parseInt(maxOrech)
 	if(!isNaN(num)) {
 		maxOrech=num
 
 	}
+		}
+	} catch(e){}
+	
 	var meta=$i.$_GET.meta||$i.$_GET.stats
 	return {
 		page: $i.$_GET.page || 1,
