13f971d641db8decfc7127062e9fad1dceae18ec
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-03-15 05:14:35 +0000

    B"H

--- geelooy/scripts/tricks/parsers/wikisource/tanach/getAllTanach.js
+++ geelooy/scripts/tricks/parsers/wikisource/tanach/getAllTanach.js
@@ -1,6 +1,6 @@
 //B"H
 console.log("B\"H")
-async function downloadAllTenachIndecies() {
+async function downloadAllTenachIndecies(getOnlyIndecies=true, slow=false) {
 	var t = document.querySelector("table")
 	TanachRows = Array.from(t.rows).slice(1,4);
 	var subTables = TanachRows.map(w=>w.querySelector("table"))
@@ -28,10 +28,32 @@ async function downloadAllTenachIndecies() {
 			**/
 			for(var z = 0; z < index.length; z++) {
 				console.log("Getting chapter verses",index[z])
-				var versesIndex = await getChapterSectionsIndex(index[z].href)
-				index[z] = {
-					link: aToObj(index[z]),
-					sections: versesIndex.map(r=>aToObj(r))
+				var versesIndex = await getChapterSectionsIndex(
+					index[z].href,
+					!slow
+				);
+
+
+				if(slow) {
+					if(getOnlyIndecies)
+						index[z] = {
+							link: aToObj(index[z]),
+							sections: versesIndex.map(r=>aToObj(r))
+						}
+					else {
+						for(var v = 0; v < versesIndex.length; v++) {
+							var details = await getVerseDetailsSLOW(versesIndex[v].href);
+							versesIndex[v] = {
+								link: aToObj(versesIndex[v]),
+								content: details
+							}
+						}
+					}
+				} else {
+					index[z] = {
+						link: aToObj(index[z]),
+						content: versesIndex
+					}
 				}
 			}
 			
@@ -50,12 +72,74 @@ async function downloadAllTenachIndecies() {
 	
 }
 
+/*slow version*/
+async function getVerseDetailsSLOW(url) {
+	var d = await doc(url)
+	//B"H
+	var lan= d.querySelector("div[lang='hbo']")
+	var com = d.querySelector(`a[title*='מ"ג']`)
+	if(lan && com)
+		return {
+			verse: lan.innerHTML,
+			commentaries: com.href
+		}
+	else return null
+}
+
+function getVerseDetails(chapterDoc) {
+	//B"H
+
+	function getVerses() {
+		
+		var h = chapterDoc.querySelector("div[lang='hbo']")
+		var p = h.children[0]
+		var verses = {};
+		var curVerse = null;
+		var curVerseContents = ""
+		Array.from(p.childNodes)
+		.forEach((w,i,ar) => {
+			//console.log(w.textContent)
+			if(w.tagName == "SPAN" && w.id) {
+				if(curVerse) {
+					verses[curVerse] = curVerseContents
+				}
+				console.log("OK",w.id,curVerse,curVerseContents)
+				curVerse = w.id;
+				curVerseContents = ""
+				return;
+			}
+
+			curVerseContents += w.innerHTML || w.textContent
+
+			if(i == ar.length - 1){
+				if(curVerse) {
+					verses[curVerse] = curVerseContents
+				}
+			}
+
+
+		})
+		return verses;
+	}
+
+	function getCommentaries() {
+		var m = chapterDoc.querySelector(`a[title*='מ"ג איכה ב']`)
+		return m;
+	}
+
+	return {
+		verses: getVerses(),
+		commentaries: getCommentaries()
+	}
+}
 function aToObj(a) {
 	return {
 		title: a.textContent,
 		href: a.href
 	}
 }
+
+
 async function getBookChaptersIndex(url) {
 	var d = await doc(url);
 	try {
@@ -70,19 +154,27 @@ async function getBookChaptersIndex(url) {
 	}
 }
 
-async function getChapterSectionsIndex(url) {
-	var d = await doc(url);
-	try {
-		//B"H
-		var f=d.getElementById("mw-content-text")
-		var navA = Array.from(f.querySelectorAll(".NavFrame a.mw-redirect"))
 
-		var a = Array.from(f.querySelectorAll("a.mw-redirect"))
-			.filter(w=>
-				!navA.includes(w) &&
-				w.parentNode.tagName == "SPAN"
-			)
-		return a;
+async function getChapterSectionsIndex(url, details = false) {
+	var d = await doc(url);
+
+	try {
+		if(!details) {
+			//B"H
+			var f=d.getElementById("mw-content-text")
+			var navA = Array.from(f.querySelectorAll(".NavFrame a.mw-redirect"))
+
+			var a = Array.from(f.querySelectorAll("a.mw-redirect"))
+				.filter(w=>
+					!navA.includes(w) &&
+					w.parentNode.tagName == "SPAN"
+				)
+			return a;
+
+		} else {
+			var chapter = getVerseDetails(d)
+			return chapter
+		}
 	} catch(e) {
 		console.log("There was an issue!", e)
 		return null;
