ac093037d23d88b9b13543e79cd49e29cf488187
ymerkos <ykaufer@merkos302.com>
2024-04-08 23:23:02 -0400

    B"H

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/dvarim/wheat.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/dvarim/wheat.js
@@ -3,19 +3,12 @@ B"H
 */
 
 
-import Tzomayach from "../chayim/tzomayach.js";
+import CollectableItem from "../chayim/collectableItem.js";
 
-export default class Wheat extends Tzomayach {
+export default class Wheat extends CollectableItem {
     static iconId = "wheat"
     constructor(op) {
         super(op);
         this.placeholderName = "wheat";
-        this.proximity = 0.7;
-        
-        this.visible = false;//just that which goes over wheat for now
-        this.on("nivraNeechnas", nivra => {
-            this.ayshPeula("collected", this, nivra);
-            this.olam.sealayk(this);
-        });
     }
 }
