90baaf03085bb00cb3c4f699e1d7a9744739dae7
ymerkos <ykaufer@merkos302.com>
2024-04-10 01:32:19 -0400

    B"H

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/postProcessing/minimap.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/postProcessing/minimap.js
@@ -135,7 +135,7 @@ export default class MinimapPostprocessing extends Heeooleey {
             parent: "map overlays " + category,
             className: "overlayItem",
             shaym: "item "+item.shaym,
-            onmouseover(e) {
+            onclick(e) {
                 console.log("Hovering!",item)
             },
             style: {
