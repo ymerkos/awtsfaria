c08c1e66a291978899ce3d3c07f96f443342d809
ymerkos <ykaufer@merkos302.com>
2024-04-09 23:47:44 -0400

    B"H

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/chayim/medabeir.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/chayim/medabeir.js
@@ -175,6 +175,7 @@ export default class Medabeir extends Chai {
             var d = this?.dialogue?.shlichuseem;
             if(!d) return false;
             var isAvailable = this.ayshPeula("is shlichus available", d);
+            console.log("Available?",this,isAvailable,d)
             if(isAvailable === false) {
                 var g = await this.ayshPeula("change icon style", {
                     selector: ".ikar",
