8ec5de63e0b7f5f465d195b4c0623c69f4ffeb8e
ymerkos <ykaufer@merkos302.com>
2024-04-11 19:26:16 -0400

    B"H Scroll better

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/worldLoader.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/worldLoader.js
@@ -430,6 +430,10 @@ export default class Olam extends AWTSMOOS.Nivra {
                 
             })
 
+            this.on("update minimap scroll", ({center}) => {
+                    console.log("SCrolling",center)
+            });
+
             this.on("update minimap camera", ({position, rotation, targetPosition}) => {
                 if(!this.minimap) {
                     return;
