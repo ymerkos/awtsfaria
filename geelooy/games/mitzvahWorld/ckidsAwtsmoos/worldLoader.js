94e566af562a75cc4034cb306e84ad8b8386e84c
ymerkos <ykaufer@merkos302.com>
2024-04-10 20:59:59 -0400

    B"H percent

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/worldLoader.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/worldLoader.js
@@ -2017,7 +2017,7 @@ export default class Olam extends AWTSMOOS.Nivra {
                                 })
                                 var time = Date.now() - lastTime
                                 lastTime = Date.now()
-                                console.log("Loading progress!",loaded,total, time,derech)
+                                console.log("Loading progress!",percent.nivrayimLng, time,derech)
                             }, error => {
                                 console.log(error);
                                 r();
