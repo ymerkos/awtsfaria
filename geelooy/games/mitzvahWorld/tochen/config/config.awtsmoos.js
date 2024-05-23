9b8930decb413e45990ab18151d6b2d42f7f1724
ymerkos <ykaufer@merkos302.com>
2024-05-22 21:39:21 -0400

    B"H Toechein

--- geelooy/games/mitzvahWorld/tochen/config/config.awtsmoos.js
+++ geelooy/games/mitzvahWorld/tochen/config/config.awtsmoos.js
@@ -1,7 +1,7 @@
 /**B"H*/
 export default {
 	startingLevel: 
-	"/../worlds/promisedLand.js"
+	"/../tochen/worlds/promisedLand.js"
 	//"https://firebasestorage.googleapis.com/v0/b/ckids-games.appspot.com/o/chawfawtseem%2Findexes%2Fworlds%2FpromisedLand%2FpromisedLand.js?alt=media&token=729deb4f-df2b-4936-8dff-c8dbdde95c44"
 	//"https://firebasestorage.googleapis.com/v0/b/ckids-games.appspot.com/o/chawfawtseem%2Findexes%2Fworlds%2Fcity1%2Fcity1.js?alt=media"
 	//"https://firebasestorage.googleapis.com/v0/b/ckids-games.appspot.com/o/chawfawtseem%2Findexes%2Fworlds%2Fdesert1%2Fdesert1.js?alt=media"
