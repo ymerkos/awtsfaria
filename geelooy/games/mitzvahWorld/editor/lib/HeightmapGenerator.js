90ae616272bdc260ffb81d3df5b26771c3bceef9
ymerkos <ykaufer@merkos302.com>
2024-05-23 13:02:09 -0400

    B"H Heightining

--- geelooy/games/mitzvahWorld/editor/lib/HeightmapGenerator.js
+++ geelooy/games/mitzvahWorld/editor/lib/HeightmapGenerator.js
@@ -40,6 +40,14 @@ export default class HeightMapGenerator {
 
         // Add the orthographic camera to the scene
         this.scene.add(this.orthoCamera);
+
+        // Set the mesh to a specific layer
+        this.mesh.layers.set(1);
+        this.scene.traverse((node) => {
+            if (node instanceof THREE.Mesh && node !== this.mesh) {
+                node.layers.disable(1);
+            }
+        });
     }
 
     setupRenderTarget() {
@@ -56,14 +64,14 @@ export default class HeightMapGenerator {
                 cameraNear: { value: this.orthoCamera.near },
                 cameraFar: { value: this.orthoCamera.far }
             },
-            vertexShader: /*glsl*/`
+            vertexShader: `
                 varying vec4 vWorldPosition;
                 void main() {
                     vWorldPosition = modelMatrix * vec4(position, 1.0);
                     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
                 }
             `,
-            fragmentShader: /*glsl*/`
+            fragmentShader: `
                 uniform float cameraNear;
                 uniform float cameraFar;
                 varying vec4 vWorldPosition;
@@ -81,21 +89,12 @@ export default class HeightMapGenerator {
             side: THREE.DoubleSide
         });
 
-        // Set the mesh to a specific layer
-        const originalLayer = this.mesh.layers.mask;
-        const heightMapLayer = 1;
-        this.mesh.layers.set(heightMapLayer);
-        this.orthoCamera.layers.enable(heightMapLayer);
-
         this.renderer.setSize(this.mapWidth, this.mapHeight);
         this.renderer.setRenderTarget(this.renderTarget);
         this.scene.overrideMaterial = depthMaterial;
         this.renderer.render(this.scene, this.orthoCamera);
         this.scene.overrideMaterial = null;
         this.renderer.setRenderTarget(null);
-
-        // Restore the original layer
-        this.mesh.layers.mask = originalLayer;
 
         const readPixels = new Uint8Array(this.mapWidth * this.mapHeight * 4);
         this.renderer.readRenderTargetPixels(this.renderTarget, 0, 0, this.mapWidth, this.mapHeight, readPixels);
@@ -107,6 +106,35 @@ export default class HeightMapGenerator {
             const b = readPixels[i * 4 + 2];
             const depth = (r + g + b) / 3;
             this.heightMap[i] = depth;
+        }
+    }
+
+    getHeightAt(x, z, boundingBox) {
+        const worldWidth = boundingBox.max.x - boundingBox.min.x;
+        const worldHeight = boundingBox.max.z - boundingBox.min.z;
+        const cellWidth = worldWidth / this.mapWidth;
+        const cellHeight = worldHeight / this.mapHeight;
+
+        const gridX = Math.floor((x - boundingBox.min.x) / cellWidth);
+        const gridZ = Math.floor((z - boundingBox.min.z) / cellHeight);
+
+        if (gridX < 0 || gridX >= this.mapWidth || gridZ < 0 || gridZ >= this.mapHeight) {
+            return boundingBox.min.y;
+        }
+
+        const heightIndex = gridZ * this.mapWidth + gridX;
+        const heightValue = this.heightMap[heightIndex];
+        const normalizedHeight = heightValue / 255;
+        const worldHeightValue = normalizedHeight * (boundingBox.max.y - boundingBox.min.y) + boundingBox.min.y;
+
+        return worldHeightValue;
+    }
+
+    updateObjectPosition(object, boundingBox) {
+        const position = object.position;
+        const height = this.getHeightAt(position.x, position.z, boundingBox);
+        if (!object.isJumping) {
+            position.y = height;
         }
     }
 
@@ -161,32 +189,5 @@ export default class HeightMapGenerator {
         ]))
     }
 
-    getHeightAt(x, z, boundingBox) {
-        const worldWidth = boundingBox.max.x - boundingBox.min.x;
-        const worldHeight = boundingBox.max.z - boundingBox.min.z;
-        const cellWidth = worldWidth / this.mapWidth;
-        const cellHeight = worldHeight / this.mapHeight;
 
-        const gridX = Math.floor((x - boundingBox.min.x) / cellWidth);
-        const gridZ = Math.floor((z - boundingBox.min.z) / cellHeight);
-
-        if (gridX < 0 || gridX >= this.mapWidth || gridZ < 0 || gridZ >= this.mapHeight) {
-            return boundingBox.min.y;
-        }
-
-        const heightIndex = gridZ * this.mapWidth + gridX;
-        const heightValue = this.heightMap[heightIndex];
-        const normalizedHeight = heightValue / 255;
-        const worldHeightValue = normalizedHeight * (boundingBox.max.y - boundingBox.min.y) + boundingBox.min.y;
-
-        return worldHeightValue;
-    }
-
-    updateObjectPosition(object, boundingBox) {
-        const position = object.position;
-        const height = this.getHeightAt(position.x, position.z, boundingBox);
-        if (!object.isJumping) {
-            position.y = height;
-        }
-    }
 }
