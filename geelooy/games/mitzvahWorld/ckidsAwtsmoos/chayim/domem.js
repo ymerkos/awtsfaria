1b8f870cc50d8fa8f9ba4641e7b477a664d603b6
ymerkos <ykaufer@merkos302.com>
2024-06-04 18:15:10 -0400

    B"H grass

--- geelooy/games/mitzvahWorld/ckidsAwtsmoos/chayim/domem.js
+++ geelooy/games/mitzvahWorld/ckidsAwtsmoos/chayim/domem.js
@@ -605,7 +605,7 @@ export default class Domem extends Nivra {
                 Math.random() * Math.PI * 2.0,
                 0.0,
               ),
-              new THREE.Vector3().setScalar(0.27),
+              new THREE.Vector3().setScalar(Math.random() * .27 + 0.17),
             );
           }
     }
