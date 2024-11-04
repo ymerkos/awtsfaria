b0481380568b9927f918a09609f09c723442410f
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-04 01:54:40 -0500

    B"H

---
+++ geelooy/scripts/tricks/openaihacks/codes.js
@@ -0,0 +1,15 @@
+//B"H
+//B"H
+async function awtsmoosifyOpenaiTokens() {
+    g=await import("https://cdn.oaistatic.com/assets/i5bamk05qmvsi6c3.js")
+    z = await g.bk() //chat requirements
+
+    r =  await g.bi(z.turnstile.bx) //turnstyle token
+    arkose = await g.bl.getEnforcementToken(z)
+    p = await g.bm.getEnforcementToken(z) //p token
+
+    //A = fo(e.chatReq, l ?? e.arkoseToken, e.turnstileToken, e.proofToken, null)
+    
+    return g.fX(z,arkose, r, p, null)
+}
+//f=await awtsmoosifyTokens()
