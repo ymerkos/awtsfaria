41c40be0e21a5448692663b0af46a707bd25b04a
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-08 08:38:01 -0500

    B"H

---
+++ geelooy/scripts/awtsmoos/api/helperScripts/traverseAllPosts.js
@@ -0,0 +1,90 @@
+//B"H
+//B"H
+var base = "/api/social"
+async function addParentIdToAllPostsInHeichel({
+    heichelId,
+    aliasId,
+    callback
+}) {
+    
+   
+    await traverseAllPostsInSeries({
+        seriesId: "root",
+        heichelId,
+        callback
+    })
+    
+}
+
+
+
+async function traverseAllPostsInSeries({
+    seriesId,
+    heichelId,
+    callback
+}) {
+    var rootSubSeries = await get(`${
+        base
+    }/heichelos/${heichelId}/series/${seriesId}/series`)
+    var rootPosts = await get(base+
+        `/heichelos/${heichelId}/series/${seriesId}/posts`
+    );
+
+    for(var rootPost of rootPosts) {
+        if(typeof(callback) == "function") {
+            await callback({post: rootPost, seriesId})
+        }
+    }
+
+    for(var rootSs of rootSubSeries) {
+        await traverseAllPostsInSeries({
+            seriesId: rootSs,
+            heichelId,
+            callback
+        })
+        
+    }
+}
+
+async function editPost({
+    heichelId,
+    aliasId,
+    postId,
+    details={}
+}) {
+    return await get(
+        base+
+        `/heichelos/${heichelId}/post/${postId}`, {
+            method: "PUT",
+            body: new URLSearchParams(details)
+        }
+    )
+}
+
+async function get(url, opts={}) {
+    return (await fetch(url, opts)).json()
+}
+
+/*
+var times  = 0;
+await addParentIdToAllPostsInHeichel({
+    
+    heichelId:"ikar", async callback(r) {
+        times++;
+        if(times > 5) return console.log("DONE testing")
+        k=await editPost({
+            postId: r.post,
+            heichelId: "testi",
+            details: {
+
+                aliasId: "sefarim",
+                
+                parentSeriesId: r.seriesId,
+                dontOverride: true
+            }
+        })
+        console.log("YO",r,k)
+    }
+})
+*/
+export default traverseAllPostsInSeries;
