e33753b0c8010a4160c9ec19b88b76bd68798507
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-08 09:38:52 -0500

    B"H

---
+++ ayzarim/awtsmoosDynamicServer/tests/streaming.js
@@ -0,0 +1,16 @@
+//B"H
+/*testing if streaming works*/
+var http = require("http")
+http.createServer((q,r) => {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Transfer-Encoding': 'chunked',
+      'Awtsmoos',"yes"
+  });
+  for(var i = 0; i < 9; i++) {
+			await new Promise(r =>(setTimeout(() => {r()}, 300)));
+			r.write("WOW "+ i);
+		}
+		r.end("LOL");
+  
+}).listen(8081)
