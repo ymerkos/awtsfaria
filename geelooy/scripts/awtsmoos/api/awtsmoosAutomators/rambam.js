f08044645c7dd195225aeaf99cdb99233eeaefcf
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-10 19:51:53 -0500

    B"H

--- geelooy/scripts/awtsmoos/api/awtsmoosAutomators/rambam.js
+++ geelooy/scripts/awtsmoos/api/awtsmoosAutomators/rambam.js
@@ -8,6 +8,19 @@ async function addRambamToAwtsmoos(rambamJSON) {
 	for(var sefer in rambamJSON) {
 		var seferTitle = sefer.header
 		var seferDescription = sefer.info;
+		var f=await makeSeries({
+		    seriesName: seferTitle,
+		    heichelId: "ikar",
+		    aliasId: "rambam",
+		    description: seferDescription,
+		    parentSeries: "BH_1731270366023_715_sefarim"
+		})
+		var seferSeriesId = f?.success?.newSeriesID;
+		if(!seferSeriesId) {
+			console.log("Sefer series NOT made!",f,sefer);
+			return;
+		}
+		
 		for(var halacha in sefer.children) {
 			/**
 				new sub series
@@ -16,6 +29,18 @@ async function addRambamToAwtsmoos(rambamJSON) {
 				halacha.header;
 			var halachaDescription =
 				halacha.content[0].connections;
+			var h=await makeSeries({
+			    seriesName: halachaTitle,
+			    heichelId: "ikar",
+			    aliasId: "rambam",
+			    description: halachaDescription,
+			    parentSeries: seferSeriesId
+			})
+			var halachaSeriesId = f?.success?.newSeriesID;
+			if(!halachaSeriesId) {
+				console.log("halacha series NOT made!",h, halacha);
+				return;
+			}
 			for(
 				var chapter in halacha
 					.content
@@ -26,7 +51,21 @@ async function addRambamToAwtsmoos(rambamJSON) {
 				var halachos =
 					chapter.content.find(w =>
 						w.name == "דפוס"
-					)
+					);
+				var connections = chapter.content[0]?.connections;
+
+				f=await makePost({
+					postName: chapterTitle,
+					heichelId: "ikar",
+					aliasId: "rambam",
+					sections:halachos,
+					parentSeries: halachaSeriesId,
+					dayuh: {
+						meta: {
+							connections
+						}
+					}
+				})
 			}
 		}
 	}
