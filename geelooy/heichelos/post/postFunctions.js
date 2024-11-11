fb4f48b0ad7eaecc17a0ce1f3d51223b60d7d273
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-11 01:52:20 -0500

    B"H

--- geelooy/heichelos/post/postFunctions.js
+++ geelooy/heichelos/post/postFunctions.js
@@ -21,7 +21,7 @@ function highlightActiveDiv(elId, targetClass, callback) {
     const parentHeight = parentDiv.clientHeight;
     var middleThreshold = scrollTop + (parentHeight * 0.8); // Middle of the visible area
     var offset = 50; // Distance below the middle threshold to activate
-	if(innerWidth < 769) {
+	if(innerWidth < 769 && parentHeight < 120) {
 		middleThreshold = scrollTop + (parentHeight * 2);
 	}
     const adjustedThreshold = middleThreshold + offset; // Adjusted threshold
