f0ce98588ed5f5abbba5785785903a7c1ce9df7a
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-18 02:00:21 -0500

    B"H
    tabloid

--- geelooy/heichelos/post/postFunctions.js
+++ geelooy/heichelos/post/postFunctions.js
@@ -1,6 +1,6 @@
 //B"H
 import Highlighter from "/api/nav/highlighter.js";
-import TabManager from "/heichelos/post/TabManager-1.js";
+import TabManager from "/heichelos/post/TabManager.js";
 
 var seriesInfo = {}
 var otherPostInfo = {}
