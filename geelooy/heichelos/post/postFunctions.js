4129b0e95c6ff933d7386c05f1cdec2f80582351
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-11-17 22:38:13 -0500

    B"H
    add tab new

--- geelooy/heichelos/post/postFunctions.js
+++ geelooy/heichelos/post/postFunctions.js
@@ -1,5 +1,7 @@
 //B"H
 import Highlighter from "/api/nav/highlighter.js";
+import TagManager from "/heichelos/post/TagManager.js";
+
 var seriesInfo = {}
 var otherPostInfo = {}
 window.seriesInfo=seriesInfo
@@ -310,6 +312,7 @@ function makeNavBars(post, seriesParent,
 		`>`;
 	return html;
 }
+var man = null;
 
 
 function addTab({
@@ -326,136 +329,23 @@ function addTab({
 	onclose,
 	oninit
 }) {
-	if (
-	
-		!parent ||
-		!btnParent
-	) return console.log("Need to supply 'parent' and 'btnParent' params");
-	
-	var par = parent;
-	var btnPar = btnParent;
-	
-	var btn = document.createElement(
-		"div");
-	btn.className = "tab-button"
-	btnPar.appendChild(btn);
-	btn.textContent = header;
-	
-	
-	var tab = document.createElement(
-		"div");
-	tab.awtsTabBtn = btn;
-	tab.className = "tab-container";
-	par.appendChild(tab);
-	
-	var info = document.createElement(
-		"div")
-	info.className = "post-info";
-	tab.appendChild(info)
-
-	var commentHeader = document.createElement("div")
-	commentHeader.classList.add("comment-header")
-	info.appendChild(commentHeader);
-	
-	var bck = document.createElement(
-		"div");
-	bck.className = "back-btn";
-	bck.textContent = "Back";
-	commentHeader.appendChild(bck);
-	
-	var hdr = document.createElement(
-		"div");
-	hdr.className = "info-header";
-	hdr.textContent = header;
-	commentHeader.appendChild(hdr);
-	tab.awtsHeader = hdr;
-	
-	var actualTab = document
-		.createElement("div");
-	actualTab.className = "tab-content";
-	tab.actual = actualTab;
-	if (content)
-		appendHTML(content, actualTab);
-	if (typeof (append) == "function") {
-		append(actualTab)
+	if(!man) {
+		man = new TagManager()
 	}
-	//       actualTab.innerHTML = content;
-	info.appendChild(actualTab);
-	var tabHidden = true;
-	var tabParent = (tabParent ||
-		btnPar);
-	btn.onclick = async () => {
+	return man.addTab({
 		
-		tabParent.classList.add(
-			"backScreen")
-		if (!addClasses)
-			btnPar.classList
-			.remove("active")
-		par.classList.add(
-			"active");
-		
-		if (!addClasses)
-			Array.from(par
-				.children)
-			.forEach(n => {
-				n.classList
-					.remove(
-						"active"
-					)
-				
-			});
-		tab.classList.add(
-			"active");
-		if (typeof (onopen) ==
-			"function") {
-			await onopen({
-				tab,
-				actualTab
-			})
-		}
-		
-	}
-	
-	bck.onclick = async () => {
-		tabParent.classList
-			.remove(
-				"backScreen");
-		tabParent.classList.add(
-			"active");
-		onswitch?.({tab: tabParent})
-		// if(addClasses) {
-		tab.classList.remove(
-			"active")
-		if (typeof (onclose) ==
-			"function") {
-			await onclose({
-				tab,
-				actualTab
-			})
-		}
-		actualTab.innerHTML = "";
-		
-		// par.classList.remove("active");
-		
-		/*setTimeout(()=>{
-		    tab.classList.remove("active")
-
-		}, 1000)*/
-		;
-	}
-	oninit?.(tab);
-	tab.awtsRefresh = () => {
-		actualTab.innerHTML = "";
-		onopen?.({
-				tab,
-				actualTab
-			});
-	};
-	tab.onUpdateHeader = /*change header*/ (header) => {
-		tab.awtsHeader.innerText = header;
-		tab.awtsTabBtn.innerText = header;
-	}
-	return tab;
+		header,
+		content,
+		append,
+		addClasses = false,
+		parent = null,
+		btnParent = null,
+		tabParent = null,
+		onswitch,
+		onopen,
+		onclose,
+		oninit
+	})
 }
 
 function updateQueryStringParameter(key, value) {
