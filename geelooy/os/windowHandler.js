84f7c39b4efe4b5ea56fd425a2f52a77c002efca
ymerkos <121984148+ymerkos@users.noreply.github.com>
2024-12-25 05:50:59 -0500

    B"H

--- geelooy/os/windowHandler.js
+++ geelooy/os/windowHandler.js
@@ -1,12 +1,30 @@
 //B"H
 import ResizableWindow from "./windows.js"
+import {
+    programs,
+    programsByExtensionDefaults
+}
 export default class WindowHandler {
     windows = [];
     constructor() {
 
     }
-
+    getExtension(title) {
+        var l = title.lastIndexOf(".")
+        if(l > -1) {
+            return title.substring(l)
+        }
+        return null;
+    }
     addWindow({title, content}) {
+        var ext = this.getExtension(title);
+        var prog = programsByExtensionDefaults[ext];
+        if(prog) {
+            var program = programs[prog];
+            if(program) {
+                content = program(title, content);
+            }
+        }
         var wind = new ResizableWindow({
             title, content,
             handler: this
